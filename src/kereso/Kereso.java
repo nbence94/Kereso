b12c67ab61ac68d19ec40a15e8b0f2579e24d0cd
nbence94 <nbence094@gmail.com>
2020-07-09 11:46:23 +0200

    Keres funkció béta

--- src/kereso/Kereso.java
+++ src/kereso/Kereso.java
@@ -9,32 +9,66 @@ import java.io.BufferedReader;
 public class Kereso {
     
     private String path;
-    private int fileLines;
+    private int tombLength;
     
     public Kereso(String filePath){
         this.path = filePath;
     }
     
-    public int Lines() throws IOException{
+    public void Lines() throws IOException{
         FileReader fr = new FileReader(path);
         BufferedReader br = new BufferedReader(fr); 
         int count = 0;
         while (br.readLine() != null)  count++;        
         br.close();
-        return count;
+        tombLength = count;
     }
     
-    
-    public String[] readUp() throws IOException{
+    public String[][] readUp() throws IOException{
         FileReader fr = new FileReader(path);
         BufferedReader br = new BufferedReader(fr); 
-        int linesNum = Lines();
-        String[] Datas = new String[linesNum];
+        Lines();
+        int linesNum = tombLength;
+        String[][] Datas = new String[linesNum][4];
         for(int i = 0; i < linesNum; i++){
-            Datas[i] = br.readLine();
-        }       
+            String[] sor = br.readLine().split("\\|");
+            Datas[i][0] = sor[0];
+            Datas[i][1] = sor[1];
+            Datas[i][2] = sor[2];
+            Datas[i][3] = sor[3];    
+        }
         br.close();
         return Datas;
     }
+    
+       public String[] olvass() throws IOException{
+        FileReader fr = new FileReader(path);
+        BufferedReader br = new BufferedReader(fr); 
+        int linesNum = tombLength;
+        String[] Datas = new String[linesNum];
+        for(int i = 0; i < linesNum; i++){
+            Datas[i] = br.readLine();
+        }
+        br.close();
+        return Datas;
+    } 
+    
+    
+    public void Keres(String[][] places, String searchThis){
+        for(int i = 0; i < tombLength; i++) {
+            if(places[i][0].toLowerCase().equals(searchThis.toLowerCase())){
+                System.out.println("> " + places[i][0]);
+                System.out.println("- Zóna: " + places[i][1]);
+                System.out.println("- Épület: " + places[i][2]);
+                System.out.println("Egyéb információ: \n" + places[i][3]);
+            }
+            else if(places[i][0].toLowerCase().contains(searchThis.toLowerCase())){
+                System.out.println("> " + places[i][0]);
+            }
+        }
+    }
+      
+    
+    
     
 }
